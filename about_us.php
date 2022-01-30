b021d9a79b4280ddc2f543328ca4bb6284f5a91b
Saie Suraj vadke <92719904+saie-vadke@users.noreply.github.com>
2022-01-30 20:28:40 +0530

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+include("connection.php");
+include("header.php");
+?>
+
+
+
+
+
+
+
+
+
+
+
+<div class="row">
+<div class="col-lg-7" >
+<h1 style="color:#609"> About College Allumni Portal</h1>
+<img src="img/U158P5029T49D36F737DT20130626151845.jpg" width="50%"; style=" float:right; border:#999 solid"/>
+<p>Alumni portals is providing common platform for every institute. Owing to the need to have all the Alumni (already passed out students) must be connected to the Institute,resulted in sharing their experiences , views , ideas, guidance , motivations and strategies.
+</p>
+
+<p> The objective of Alumni portal application is to allow old and new students of the college to communicate with each other. This allows students to know about each other and their current activities. </p>
+<p>This portal highlights the feature of communication, which will enable the current students to have interaction with the alumni of the college for getting various updates on current indusrty trends, Internship opportunity, sponsored projects and various referral opening in the corporate world.
+</p>
+<p> This portal will serve the cause of integrating all the stakeholders of Institute such as , Alumni, College students , Faculties to avail the guidance and knowledge sharing on various domains. In this project we proposed the Dynamic Architectural design of the Alumni portal, which enable the two way communication between all stakeholders.
+
+ 
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+ <script src="vendor/jquery/jquery.min.js"></script> 
+
+<!-- Bootstrap Core JavaScript --> 
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script> 
+
+<!-- Metis Menu Plugin JavaScript --> 
+<script src="vendor/metisMenu/metisMenu.min.js"></script> 
+
+<!-- Morris Charts JavaScript --> 
+<script src="vendor/raphael/raphael.min.js"></script> 
+<script src="vendor/morrisjs/morris.min.js"></script> 
+<script src="data/morris-data.js"></script> 
+
+<!-- Custom Theme JavaScript --> 
+<script src="dist/js/sb-admin-2.js"></script>
+</body>
+</html>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
